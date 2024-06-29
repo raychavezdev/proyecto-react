5ee7cac5c15a8d9f4dd8aa4b056d03bd7a2edb0c
Raymundo Chavez <raymundo.developer@gmail.com>
2024-06-29 15:45:33 -0600

    Se da estilos a la lista de criptos

--- src/Cripto.jsx
+++ src/Cripto.jsx
@@ -1,12 +1,24 @@
-const Cripto = ({index,name,priceUsd}) => {
+const Cripto = ({ name, priceUsd, symbol, changePercent24Hr }) => {
   return (
-    <li className="cripto" >
+    <li className="cripto">
+      <p>
+        <span className="cripto-name">{name}</span>
+      </p>
+      <p>
+        Precio <span> $ {parseFloat(priceUsd).toFixed(4)}</span>
+      </p>
 
-      <span>{name}</span>
-      <span> $ {priceUsd}</span>
+      <p>
+        Código: <span>{symbol}</span>
+      </p>
+      <p>
+        Var. 24hrs:
+        <span className={changePercent24Hr > 0 ? "cripto-up" : "cripto-down"}>
+          {parseFloat(changePercent24Hr).toFixed(3)} %
+        </span>
+      </p>
     </li>
   );
 };
 
-
-export default Cripto
+export default Cripto;
